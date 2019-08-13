ced7abedddf89649ff78aa3dbf1a931e9aea1c6c
张发亮 <fl.zhang@8win.com>
2019-08-13 15:37:23 +0800

    add laylot

--- Layouts/globalLayout/index.js
+++ Layouts/globalLayout/index.js
@@ -9,79 +9,54 @@ export default class SiderDemo extends React.Component {
     collapsed: false
   };
 
-  onCollapse = collapsed => {
-    console.log(collapsed);
-    this.setState({ collapsed });
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
   };
 
   render() {
     const { children } = this.props;
     return (
-      <Row className={styles.layout} gutter={24}>
-        <Col span={24}>
-          <Layout style={{ minHeight: "100vh" }}>
-            <Sider
-              collapsible
-              collapsed={this.state.collapsed}
-              onCollapse={this.onCollapse}
+      <Row className={styles.layout}>
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span>nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span>nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>nav 3</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: "#fff", padding: 0 }}>
+              <Icon
+                className="trigger"
+                type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+                onClick={this.toggle}
+              />
+            </Header>
+            <Content
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                background: "#fff",
+                minHeight: 280
+              }}
             >
-              <div className="logo" />
-              <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-                <Menu.Item key="1">
-                  <Icon type="pie-chart" />
-                  <span>Option 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="desktop" />
-                  <span>Option 2</span>
-                </Menu.Item>
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="user" />
-                      <span>User</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item key="3">Tom</Menu.Item>
-                  <Menu.Item key="4">Bill</Menu.Item>
-                  <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
-                      <Icon type="team" />
-                      <span>Team</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item key="6">Team 1</Menu.Item>
-                  <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9">
-                  <Icon type="file" />
-                  <span>File</span>
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout>
-              <Header   className={styles.header} />
-              <Content style={{ margin: "0 16px" }}>
-                <div
-                className={styles.main}
-                  style={{ padding: 24, background: "#fff", minHeight: 360 }}
-                >
-                  {children}
-                </div>
-              </Content>
-              <Footer  className={styles.footer}>
-               footer
-              </Footer>
-            </Layout>
+             {children}
+            </Content>
           </Layout>
-        </Col>
+        </Layout>
       </Row>
     );
   }
