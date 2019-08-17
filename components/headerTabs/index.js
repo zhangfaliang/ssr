59d7a7503ea0752c070cf6c47d74cb3fcf81b011
张发亮 <fl.zhang@8win.com>
2019-08-17 16:45:03 +0800

    add 滑块

--- components/headerTabs/index.js
+++ components/headerTabs/index.js
@@ -1,45 +1,28 @@
-import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
-
+import { Tabs, WhiteSpace, Badge } from "antd-mobile";
+import styles from './index.less';
 const tabs = [
-  { title: <Badge text={'3'}>直播</Badge> },
-  { title: <Badge text={'今日(20)'}>推荐</Badge> },
+  { title: <Badge text={"3"}>直播</Badge> },
+  { title: <Badge text={"今日(20)"}>推荐</Badge> },
   { title: <Badge dot>热门</Badge> },
-  { title: <Badge text={'3'}>最新</Badge> },
-  { title: <Badge text={'今日(20)'}>动漫</Badge> },
-  { title: <Badge dot>影视</Badge> },
+  { title: <Badge text={"3"}>最新</Badge> },
+  { title: <Badge text={"今日(20)"}>动漫</Badge> },
+  { title: <Badge dot>影视</Badge> }
 ];
 
-
-
 const HeaderTabs = () => (
-  <div>
-    <Tabs tabs={tabs}
+  <div className ={styles.headerBar}>
+    <Tabs
+      tabs={tabs}
       initialPage={1}
-      onChange={(tab, index) => { console.log('onChange', index, tab); }}
-      onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
-    >
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content1 of first tab
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content2 of second tab
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content3 of third tab
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content4 of first tab
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content5 of second tab
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-        Content6 of third tab
-      </div>
-    </Tabs>
+      onChange={(tab, index) => {
+        console.log("onChange", index, tab);
+      }}
+      onTabClick={(tab, index) => {
+        console.log("onTabClick", index, tab);
+      }}
+    />
     <WhiteSpace />
-
   </div>
 );
 
-export  default HeaderTabs
+export default HeaderTabs;
