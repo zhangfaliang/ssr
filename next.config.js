06b8b2bba9170e77197370725fbff533db0f8f25
张发亮 <fl.zhang@8win.com>
2019-08-17 15:50:04 +0800

    add

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withCss = require("@zeit/next-css");
 const withTM = require("next-transpile-modules");
 const withPlugins = require("next-compose-plugins");
 const withFonts = require("next-fonts");
+
 const config = process.env.NODE_ENV !== "production" ? devConfig : proConfig;
 module.exports = withPlugins(
   [withCss, withLess, withBundleAnalyzer, withTM, withFonts],
