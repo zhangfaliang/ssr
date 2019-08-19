ab21983e3e6203020354d6abc6a6c039b045a968
张发亮 <fl.zhang@8win.com>
2019-08-19 18:59:27 +0800

    add theme

--- components/recommend/selfdomTitle/index.js
+++ components/recommend/selfdomTitle/index.js
@@ -1,22 +1,43 @@
 import React, { PureComponent } from "react";
 import classnames from "classnames";
+import { get, isArray } from "lodash";
 import styles from "./index.less";
 
 class SelfdomTitle extends PureComponent {
   render() {
-    const { preFixCls } = this.props;
+    const { preFixCls, leftLabel, rightLabel, title } = this.props;
     const selfdomCls = classnames({
       [styles[`${preFixCls || "default"}-title`]]: true
     });
     return (
       <div className={selfdomCls}>
-        <div className={styles.left}> 1 </div>
+        <div className={styles.left}>
+          <div className={styles.top}>
+            {isArray(leftLabel) &&
+              leftLabel.map((item, index) => (
+                <span className={styles.leftLabel} key={index}>
+                  {item}
+                </span>
+              ))}
+            <span className={styles.title}>{title}</span>
+            {isArray(rightLabel) &&
+              rightLabel.map((item, index) => (
+                <span className={styles.rightLabel} key={index}>
+                  {item}
+                </span>
+              ))}
+          </div>
+          <div className={styles.button}> button </div>
+        </div>
         <div className={styles.right}> 2 </div>
       </div>
     );
   }
 }
 SelfdomTitle.defaultProps = {
-  preFixCls: "default"
+  preFixCls: "default",
+  title: "海贼王",
+  leftLabel: ["冒险"],
+  rightLabel: ["冒险", "奇幻"]
 };
 export default SelfdomTitle;
