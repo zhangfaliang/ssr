3217634b0de8cb52dbaa181ebbf5d652b3819626
张发亮 <fl.zhang@8win.com>
2019-09-19 18:21:33 +0800

    加密密码和账号

--- models/user/signin/sagas.js
+++ models/user/signin/sagas.js
@@ -8,18 +8,27 @@ import {
   select
 } from "redux-saga/effects";
 import es6promise from "es6-promise";
+import { get } from "lodash";
 import { sigIn } from "../../../services/user";
 import { makeInputValus } from "./selects";
 import { USER_SIGN_IN } from "./actionTypes";
-
+import sjcl from "../../../utils/sjcl";
 es6promise.polyfill();
 
 function* signIn() {
   try {
     const inputValus = yield select(makeInputValus);
-    yield call(sigIn, inputValus);
+    console.log(inputValus,'--------')
+    yield call(sigIn, {
+      userName: get(inputValus, "phoneNumber", ""),
+      password: sjcl.encrypt("password", get(inputValus, "password", "")),
+      passwordAgin: sjcl.encrypt(
+        "passwordAgin",
+        get(inputValus, "passwordAgin", "")
+      )
+    });
   } catch (err) {
-    yield put(failure(err));
+    console.log(err);
   }
 }
 
